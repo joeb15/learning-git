9b9c4f71103684d4d21ca45c9f6d1867d1ee58b7
Nicholas Wachter <nawachte@calpoly.edu>
2020-10-11 09:54:00 -0700

    fix my java class

--- src/people/Nick.java
+++ src/people/Nick.java
@@ -2,6 +2,7 @@ package people;
 
 public class Nick {
   public static int add(int x, int y) {
-    throw new UnsupportedOperationException("Not yet implemented!");
+//    throw new UnsupportedOperationException("Not yet implemented!");
+  return x+y;
   }
 }
